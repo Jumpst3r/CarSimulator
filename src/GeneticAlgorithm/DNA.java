bc8aba0fa8b65c537f7ff389f7b09e598e894f42
nicolas <nicolas.dutly@unifr.ch>
2018-04-25 20:56:44 +0200

    some tests

--- src/GeneticAlgorithm/DNA.java
+++ src/GeneticAlgorithm/DNA.java
@@ -22,10 +22,14 @@ public class DNA {
     }
 
     public DNA() {
-        for (int i = 0; i < genes.length; i++) {
+        Random r = new Random();
+        /*for (int i = 0; i < genes.length; i++) {
             Random r = new Random();
             genes[i] = 5.0 * r.nextDouble();
-        }
+        }*/
+        genes[0] = 10.0 * r.nextDouble();
+        genes[1] = 0;
+        genes[2] = 0;
         this.phenotype = getPhenotype();
         this.fitness = calculateFitness(this);
         System.out.printf("DNA PIDParams: %f, %f, %f with fitness %f\n", genes[0], genes[1], genes[2], this.fitness);
@@ -43,16 +47,16 @@ public class DNA {
             double old_error = 0;
             double integral = 0;
             double derivative;
-            double dt = 0.005;
+            double dt = 0.001;
             double val;
             int sp = 5;
             CarSimulator simulator = new CarSimulator();
             new Thread(simulator).start();
-            long stop = System.nanoTime() + TimeUnit.MILLISECONDS.toNanos(500);
+            long stop = System.nanoTime() + TimeUnit.SECONDS.toNanos(5);
             while (stop > System.nanoTime()){
                 error = sp - simulator.getSpeed();
                 integral = integral + (error * dt);
-                absErr.setAbsErr(Math.abs(absErr.getAbsErr() + (error * dt)));
+                absErr.setAbsErr(absErr.getAbsErr() + (error * dt));
                 derivative = (error - old_error) / dt;
                 val = dna.genes[0] * error + dna.genes[1] * derivative + dna.genes[2] * integral;
                 simulator.setAcceleration(val);
@@ -61,16 +65,18 @@ public class DNA {
             simulator.stop();
         }).start();
         try {
-            Thread.sleep(600);
+            Thread.sleep(6000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        absErr.setAbsErr(Math.abs(absErr.getAbsErr()));
+        System.out.println("absError: " + absErr.getAbsErr());
         if (absErr.getAbsErr() == 0.5) System.out.println("defaulted!");
-        absErr.setAbsErr(absErr.getAbsErr() / 10000);
+//        absErr.setAbsErr(absErr.getAbsErr() / 10000);
         if (absErr.getAbsErr() < 1) return 1;
         double delta =  Math.abs(1 / absErr.getAbsErr());
         if (delta > 1) return 1;
-        return delta;
+        return delta * 10;
     }
 
     /**
@@ -82,8 +88,8 @@ public class DNA {
      */
     public DNA crossOver(DNA genA) {
         double n_KP = (genA.genes[0] + this.genes[0]) / 2;
-        double n_KD = (genA.genes[1] + this.genes[1]) / 2;
-        double n_KI = (genA.genes[2] + this.genes[2]) / 2;
+        double n_KD = 0;//(genA.genes[1] + this.genes[1]) / 2;
+        double n_KI = 0;//(genA.genes[2] + this.genes[2]) / 2;
 
         DNA child = new DNA(new PIDParams(n_KP, n_KD, n_KI));
         return mutate(child);
@@ -98,7 +104,7 @@ public class DNA {
         Random rnd = new Random();
         if (rnd.nextDouble() < GeneticAlgorithm.MUTATION_RATE) {
             int i = rnd.nextInt(3);
-            child.genes[i] = 5.0 * rnd.nextDouble();
+            child.genes[0] = 5.0 * rnd.nextDouble();
             child.phenotype = new PIDParams(child.genes[0], child.genes[1], child.genes[2]);
             child.fitness = calculateFitness(child);
             System.out.println("mutated!");
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,40 @@
 import CarSimulator.CarSimulator;
 import GeneticAlgorithm.GeneticAlgorithm;
+import GeneticAlgorithm.AbsErr;
 import Jama.Matrix;
 
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        GeneticAlgorithm genAlg = new GeneticAlgorithm(50, 100);
+//        GeneticAlgorithm genAlg = new GeneticAlgorithm(50, 100);
+        double error;
+        double old_error = 0;
+        double integral = 0;
+        double derivative;
+        double dt = 0.2;
+        double val;
+        int sp = 50;
+        CarSimulator simulator = new CarSimulator();
+        new Thread(simulator).start();
+        AbsErr absErr = new AbsErr(0);
+        long stop = System.nanoTime() + TimeUnit.SECONDS.toNanos(2);
+        while (stop > System.nanoTime()){
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            error = sp - simulator.getSpeed();
+            if (error < 0) System.out.println("error negative!");
+            integral = integral + (error * dt);
+            absErr.setAbsErr(absErr.getAbsErr() + integral);
+            derivative = (error - old_error) / dt;
+            val = 8 * error;
+            simulator.setAcceleration(val);
+            old_error = error;
+            System.out.println("error: " + error + " val: " + val);
+        }
+        System.out.println("absErr: " + absErr.getAbsErr());
     }
 }
